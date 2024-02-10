ba9b675e4cd89524708a4ceb5278cf65ac4c8e73
JackQuentinForde <jackforde96@gmail.com>
2024-02-10 13:31:11 +0000

    Modified CountSubstringOccurrences method to correctly differentiate between Fizz and FizzBuzz

--- FizzBuzz.Tests/Run.cs
+++ FizzBuzz.Tests/Run.cs
@@ -9,8 +9,8 @@ namespace FizzBuzz.Tests
         [TestCase(new int[] { 3, 5 }, 1)]
         [TestCase(new int[] { 3, 5, 6 }, 2)]
         [TestCase(new int[] { 1, 3, 4 }, 1)]
-        [TestCase(new int[] { 3, 9, 15, 5, 10, 11 }, 3)]
-        [TestCase(new int[] { 3, 6, 9, 12, 61, 73, 36, 60 }, 6)]
+        [TestCase(new int[] { 3, 9, 15, 5, 10, 11 }, 2)]
+        [TestCase(new int[] { 3, 6, 9, 12, 61, 73, 36, 60 }, 5)]
         public void Outputs_Fizz_When_Numbers_Are_Divisible_By_3(int[] numbers, int expectedFizzCount)
         {
             // Arrange
@@ -33,7 +33,7 @@ namespace FizzBuzz.Tests
         [TestCase(new int[] { 5, 10 }, 2)]
         [TestCase(new int[] { 5, 10, 11, 13 }, 2)]
         [TestCase(new int[] { 5, 10, 20, 25, 35, 50 }, 6)]
-        [TestCase(new int[] { 5, 10, 20, 100, 3, 6, 36, 25, 35, 50 }, 7)]
+        [TestCase(new int[] { 5, 10, 20, 100, 3, 6, 36, 25, 35, 50, 60 }, 7)]
         public void Outputs_Buzz_When_Numbers_Are_Divisible_By_5(int[] numbers, int expectedBuzzCount)
         {
             // Arrange
@@ -101,10 +101,24 @@ namespace FizzBuzz.Tests
             int index = 0;
             while ((index = text.IndexOf(substring, index)) != -1)
             {
+                if (IsWholeWord(text, index, substring.Length))
+                {
+                    count++;
+                }
                 index += substring.Length;
-                count++;
             }
             return count;
+        }
+
+        private bool IsWholeWord(string text, int startIndex, int length)
+        {
+            // Check if substring is part of a larger word
+            if ((startIndex == 0 || !char.IsLetterOrDigit(text[startIndex - 1])) &&
+                (startIndex + length == text.Length || !char.IsLetterOrDigit(text[startIndex + length])))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
