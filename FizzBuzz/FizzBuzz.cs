a646255a84051616f3702d07193c621bd41021df
JackQuentinForde <jackforde96@gmail.com>
2024-02-10 13:19:08 +0000

    Made second FizzBuzz test pass

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -8,7 +8,7 @@
         {
             foreach (int number in numbers) 
             {
-                if (number == 15)
+                if (number % 3 == 0 && number % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
