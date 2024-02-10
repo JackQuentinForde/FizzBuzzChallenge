8fd19f6a725bb16989cb75b36a744e4b2d3c93fe
JackQuentinForde <jackforde96@gmail.com>
2024-02-10 12:27:32 +0000

    Add project files.

---
+++ FizzBuzz.Tests/Run.cs
@@ -0,0 +1,40 @@
+namespace FizzBuzz.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(new int[] { 3 }, 1)]
+        public void Outputs_Fizz_When_Numbers_Are_Divisible_By_3(int[] numbers, int expectedFizzCount)
+        {
+            // Arrange
+            var sut = new FizzBuzz();
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            sut.Run(numbers);
+            var output = stringWriter.ToString();
+
+            // Assert
+            int fizzCount = CountSubstringOccurrences(output, "Fizz");
+            Assert.That(fizzCount, Is.EqualTo(expectedFizzCount));
+        }
+
+        private int CountSubstringOccurrences(string text, string substring)
+        {
+            int count = 0;
+            int index = 0;
+            while ((index = text.IndexOf(substring, index)) != -1)
+            {
+                index += substring.Length;
+                count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public FizzBuzz() { }
+
+        public void Run(int[] numbers)
+        {
+            foreach (int number in numbers) 
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
